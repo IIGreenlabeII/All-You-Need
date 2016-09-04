a0c115425d94207250bb0cf8a30a2fcc9b974dcd
Shailin Biharie <shailin@bravoure.nl>
2016-09-04 20:19:47 +0200

    Storyboard uitwerking, icon design en Apple Watch
    Storyboard uitwerking, icon design en Apple Watch extensie toegevoegd.

---
+++ All You Need/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  All You Need
+//
+//  Created by Shailin Biharie on 04-09-16.
+//  Copyright © 2016 Shailin Biharie. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
